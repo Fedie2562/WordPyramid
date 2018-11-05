379b8cdc49b588e75b503b1c04912e3c5483a7ea
fedie2562 <fedie2562@7HGM6Y1.Student.UGDSB.ED>
2018-11-05 14:06:13 -0500

    done

--- src/wordpyramid/WordPyramid.java
+++ src/wordpyramid/WordPyramid.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Ferris D.
+ * 05/11/2018
+ * Word pyramid
  */
 
 package wordpyramid;
